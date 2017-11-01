3d3b7596a4aef1bf9b311d3eca0d6f5e2dcfe7f5
Zach Walsh <zcwalsh@ccs.neu.edu>
2017-10-31 21:24:26 -0400

    nfl api script parses data

---
+++ nfl-api-script/nfl-api.py
@@ -0,0 +1,149 @@
+import requests
+import pprint
+
+# URL strings
+api_url = 'http://api.fantasy.nfl.com/'
+weekstats_endpoint = 'players/weekstats'
+stats_endpoint = 'players/stats'
+positions_array = ['QB', 'RB', 'WR', 'TE', 'DEF', 'K']
+stat_codes = {
+    '14': 'RY', #rush yds
+    '15': 'RTD', #rush td
+    '20': 'REC', #receptions
+    '21': 'REY', #rec yds
+    '22': 'RETD', #rec td
+    '30': 'FUML', #fumbles lost
+    '5': 'PY', #pass yds
+    '6': 'PTD', #pass tds
+    '7': 'INT', #int thrown
+    '45': 'SK', #sack
+    '46': 'DINT', #int caught
+    '47': 'FR', #fum recovered
+    '62': 'DYD', #def yds
+    '54': 'PA', #pts allowed
+    '50': 'DTD', #defensive td
+    '00002': 'KRTD', #kick return td
+    '00003': 'PRTD', #punt return td
+    '49': 'SF', #safety
+
+}
+points_mapping = {
+    'RY': 0.1, #rush yds
+    'RTD': 6.0, #rush td
+    'REC': 0.0, #receptions
+    'REY': 0.1, #rec yds
+    'RETD': 6.0, #rec td
+    'FUML': -2.0, #fumbles lost
+    'PY': 0.04, #pass yds
+    'PTD': 4.0, #pass tds
+    'INT': -2.0, #int thrown
+    'SK': 1.0, #sack
+    'DINT': 2.0, #int caught
+    'FR': 2.0, #fum recovered
+    'DYD': 100000000, #def yds
+    'PA': 100000000, #pts allowed
+    'INTTD': 6.0, #pick-six
+    'FRTD': 6.0, #fumble-six
+    'KRTD': 6.0, #kick return td
+    'PRTD': 6.0, #punt return td
+    'SF': 2.0
+}
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def parse_stats(stats: dict):
+    mapped_stats = dict()
+
+    for key in stats:
+        abbr = stat_codes.get(key, None)
+        if abbr is not None:
+            mapped_stats[abbr] = int(stats.get(key))
+    return mapped_stats
+
+
+def player_points(player):
+    stats = player.get('stats', None)
+    if stats is None:
+        return 0.0
+    return points(stats)
+
+
+def points(stats: dict):
+    total_points = 0.0
+    for key in stats:
+        amount = stats.get(key, 0)
+        total_points += amount * points_mapping.get(key, 0.0)
+    return round(total_points, 2)
+
+
+def player_week(player: dict):
+    """
+
+    :type player: dict
+    """
+    player.pop('esbid')
+    player.pop('gsisPlayerId')
+    player.pop('seasonPts')
+    player.pop('seasonProjectedPts')
+    player.pop('weekPts')
+    player.pop('weekProjectedPts')
+
+    stats = player.pop('stats')
+    if (stats.get('30', None) is 0) or (stats.get('31', None) is 0):
+        print(player)
+
+    player['stats'] = parse_stats(stats)
+
+    return player
+
+
+def player_stats_for_week(position, week):
+    assert position in positions_array
+    assert type(week) == int
+    assert week >= 1, week <= 17
+    print("Getting stats for position ", position, " during week ", week)
+    payload = {'format': 'json', 'statType': 'weekStats', 'position': position, 'week': week}
+    data = nfl_request(endpoint=stats_endpoint, payload=payload)
+    print("Received")
+    pp.pprint(data['players'])
+    players = list(map(player_week, list(data['players'])))
+    return players
+
+
+def nfl_request(endpoint, payload):
+    r = requests.get(api_url + endpoint, params=payload)
+    return r.json()
+
+
+def player_tds(player):
+    stats = player.get('stats', None)
+    if stats is None:
+        return 0
+    rectds = int(stats.get('rectd', 0))
+    rushtds = int(stats.get('rushtd', 0))
+    passtds = int(stats.get('passtd', 0))
+    return rectds + rushtds + passtds
+
+
+def all_stats_for_week(week: int):
+    print("Getting stats for week ", week)
+    week_stats = dict()
+    for position in positions_array:
+        week_stats[position] = player_stats_for_week(position, week)
+    return week_stats
+
+
+def all_stats_through_week(week: int):
+    all_stats = dict()
+    for wk in range(1, week):
+        all_stats[wk] = all_stats_for_week(wk)
+    return all_stats
+
+
+#defenses = player_stats_for_week("DEF", 5)
+#pp.pprint(defenses)
+
+
+r = requests.post("https://api.nfl.com/v1/oauth/token?grant_type=client_credentials&client_id=nfl&client_secret=15232")
+pp.pprint(r.json())
+
