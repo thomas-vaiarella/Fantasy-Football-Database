5887e4af10da7614a2df8544e3fefd9e821c502e
thomas-vaiarella <vaiarella.t@husky.neu.edu>
2017-11-10 14:55:53 -0500

    Began adding procdures and functions

--- football.sql
+++ football.sql
@@ -8,18 +8,12 @@ create table scoring (
 
 );
 
-create table stat (
-	stat_id int primary key,
-    stat_name enum('RY', 'RTD', 'REC', 'REY', 'RETD', 'FUML', 'PY', 'PTD', 'INT', 'SK', 'DINT', 'FR', 'DYD', 'PA', 'DTD', 'KRTD', 'PRTD')
-);
-
 create table statvalue (
 	scoring_id int,
-    stat_id int,
+    stat_name varchar(5),
     value_pts float,
     constraint scoring_id_fk foreign key (scoring_id) references scoring (scoring_id),
-    constraint stat_id_fk foreign key (stat_id) references stat (stat_id),
-    constraint pk_statvalue primary key (stat_id, scoring_id)
+    constraint pk_statvalue primary key (stat_name, scoring_id)
 );
 
 create table league (
@@ -65,11 +59,10 @@ create table weekstats (
 
 create table statsproduced (
 	weekstats_id int,
-    stat_id int,
+    stat_name varchar(5),
     stat_total int,
     constraint weekstats_id_fk_statsproduced foreign key (weekstats_id) references weekstats (weekstats_id),
-    constraint stat_id_fk_statsproduced foreign key (stat_id) references stat (stat_id),
-    constraint statsproduced_pk primary key (stat_id, weekstats_id)
+    constraint statsproduced_pk primary key (stat_name, weekstats_id)
 );
 
 create table lineup (
@@ -88,4 +81,75 @@ create table slot (
     constraint player_id_fk_slot foreign key (player_id) references player (player_id),
     constraint slot_pk primary key (lineup_id, player_id)
 );
+
+drop procedure if exists add_player_if_not_exists;
+DELIMITER //
+create procedure add_player_if_not_exists(new_player_id int, new_player_name varchar(50), new_player_team varchar(50), new_player_position varchar(50))
+begin
+INSERT INTO player (player_id, player_name, player_team, player_position)
+VALUES (new_player_id, new_player_name, new_player_team, new_player_position);
+end //
+
+DELIMITER ;
+drop function if exists get_player_id;
+
+DELIMITER //
+create function get_player_id(given_player_id int, given_player_name varchar(50), given_player_team varchar(50), given_player_position varchar(50))
+returns int
+begin
+
+declare id int default 000000000;
+select player_id into id from player where player_id = given_player_id;
+case
+	when id  != 000000000 THEN return id;
+	else 
+		CALL add_player_if_not_exists(given_player_id, given_player_name, given_player_team, given_player_position);
+        select player_id into id from player where player_id = given_player_id;
+        return id;
+        end case;
+end //
+                
+DELIMITER ;
+drop procedure if exists add_player_week_stats;
+DELIMITER //
+create procedure add_player_week_stats(given_id int, given_week_num int)
+begin
+INSERT INTO weekstats (player_id, week_num)
+VALUES (given_id, given_week_num);
+end //
+
+DELIMITER ; 
+drop function if exists get_player_week_stats;
+DELIMITER //
+create function get_player_week_stats(given_player_id int, given_week_num int)
+returns int
+begin
+declare week_stats_id_num int default -1;
+
+select weekstats_id into week_stats_id_num 
+from weekstats 
+where player_id = given_player_id and week_num = given_week_num;
+
+case
+	when week_stats_id_num  != -1 THEN return week_stats_id_num;
+	else 
+		CALL add_player_week_stats(given_player_id, given_week_num);
+        select weekstats_id into week_stats_id_num 
+		from weekstats 
+		where player_id = given_player_id and week_num = given_week_num;
+        return week_stats_id_num;
+        end case;
+end //
+
+DELIMITER ;
+drop procedure if exists set_stats_produced;
+DELIMITER //
+create procedure set_stats_produced(given_week_stats_id int, given_stat_name varchar(5), given_stat_value int)
+begin
+INSERT INTO stats_produced
+VALUES (given_week_stats_id, given_stat_name, given_stat_value);
+end //
+
+
+
 
